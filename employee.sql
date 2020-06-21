e2d7af58e7f11f130887b6632827ab075e24757b
salonitale10 <62851107+salonitale10@users.noreply.github.com>
2020-06-21 20:10:30 +0530

    Add files via upload

---
+++ employee.sql
@@ -0,0 +1,8 @@
+
+CREATE TABLE `employee` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `address` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
